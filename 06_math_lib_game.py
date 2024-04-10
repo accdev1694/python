f2a3c1592a8ac5f42a01d16199bd5219f8ddf984
unknown <diloch111@gmail.com>
2024-04-11 00:08:55 +0100

    math lib game

---
+++ 06_math_lib_game.py
@@ -0,0 +1,11 @@
+# we will practice user inputs using a math lib game
+# this game collects information from the user and prints it inside a funny story and displays it back to them
+
+# lets create some variables
+noun1 = input("enter your first noun, a known person:")
+noun2 = input("enter your second noun, a known place:")
+noun3 = input("enter your third noun, an animal pet:")
+adj = input("enter an adjective:")
+verb = input("enter a verb:")
+
+print(f"I was walking my {noun3} down the busy {noun2} street and i saw {noun1}, {noun1} was{verb}ing {adj}")
