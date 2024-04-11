c1ab55a896aaaba5fe8ed19944043f508e4337ec
unknown <diloch111@gmail.com>
2024-04-11 01:26:21 +0100

    basic arithmetic operators

---
+++ 09_math_functions.py
@@ -0,0 +1,33 @@
+# Lets look at some basic arithmetic operators
+
+# addition (+)
+count = 0           # value of 0 assigned to count variable
+count = count + 1   # increment count by adding 1 (gives 1)
+count += 1          # augmented assignment operator; same result (gives 2)
+print(count)
+
+# subtraction (-)
+count = 0           # reset the calue of count to 0
+count -= 1          # subtract 1 from value of count (gives -1)
+print(count)
+
+# multiplication (*)
+count = 2
+count *= 2          # multiplies 2 by the value of count (gives 4)
+print(count)
+
+# division (/)
+count = 5
+count /= 2          # divides the value of count by 2 (gives 2.5)
+print(count)
+
+# exponentiation (**)
+count = 5
+count **= 2         # raises the value of count to the power of 2 (gives 25)
+print(count)
+
+# remainder (%)
+count = 10
+count %= 7          # remainder (called modulus) of the division of the value of count by 7 (gives 3)
+print(count)
+
