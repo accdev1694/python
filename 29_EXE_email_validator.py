3c9c740c38b0fa515ddcc9923477ee513857de1f
unknown <diloch111@gmail.com>
2024-04-17 22:03:55 +0100

    modify return message from invalid format

---
+++ 29_EXE_email_validator.py
@@ -0,0 +1,12 @@
+# Email Validator Exercise
+# here we create an application to text the input for an email 
+# and ensure it is indeed an email format
+
+email = input("enter your email:")
+
+while not "@" in email:
+    email = input("enter a valid email:")
+    
+domain = email[email.index("@"):]
+
+print(f"your email is ****{domain}")
