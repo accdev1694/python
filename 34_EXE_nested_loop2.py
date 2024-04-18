ab15045d8707a6ae54e83b367fac1c622eaf0903
unknown <diloch111@gmail.com>
2024-04-18 19:50:49 +0100

    create a rectangle exercise

---
+++ 34_EXE_nested_loop2.py
@@ -0,0 +1,13 @@
+# Create a Rectangle
+# we create a nested loop for dsiplay a rectangle grid
+
+# collect user input for rows, columns and shape symbol
+rows = int(input("How many rows? "))
+columns = int(input("How many columns? "))
+shape = input("Enter a shape to use: ")
+
+for char in range(rows):                # iterate over each character in rows
+    for chars in range(columns):        # nest iteration of each column caharcter
+        print(f"{shape}",  end='')      # print shape horizontally
+    print()                             # move to next line after each iteration
+    
